f330e9d9304cd0c2a2d3442a14a49069319edbdc
Zach Paul <zachpaul319@gmail.com>
2023-11-19 22:55:16 -0500

    made updates and added comments

--- taskManager.rb
+++ taskManager.rb
@@ -1,12 +1,16 @@
+############################################# TASK #############################################
 class Task
+  # object attributes. "attr_accessor" creates an instance variable AND corresponding access method to read it
   attr_accessor :title, :description, :completed
 
+  # Constructor
   def initialize(title, description)
     @title = title
     @description = description
     @completed = false
-  end
+  end # "end" keyword used to determine end of block. No colons or curly braces, just "end"
 
+  # demonstrates switch-case (case-when)
   def update(field_to_update, updated_content)
     case field_to_update
     when "title"
@@ -18,47 +22,58 @@ class Task
     end
   end
 
-  def mark_complete
+  def mark_complete # methods with zero parameters do not need parentheses
     @completed = true
   end
 
   def to_string
-    "#{@title}\n\tDescription: #{@description}"
+    "#{@title}\n\t#{@description}" # no return keyword needed. Also encapsulates string interpolation via #{}
   end
 end
 
-
+############################################# TASK LIST #############################################
 class TaskList
+  # List of Task objects
   attr_accessor :task_list
 
+  # constructor initializes empty list
   def initialize
     @task_list = []
   end
 
-  def print_tasks
-    @task_list.each_with_index do |task, idx|
-      puts "#{idx+1}. #{task.to_string}"
+  def print_all_tasks
+    @task_list.each_with_index do |task, idx| # "each_with_index" allows us to iterate over an enumerable (e.g. array). We can access both index and element
+      puts "#{idx+1}. #{task.to_string}" # when calling methods with zero parameters (e.g. to_string), parentheses are not needed
     end
   end
 
+  def print_incomplete_tasks
+    @task_list.each do |task| # Like a forEach
+      unless task.completed # "unless" similar to a guard in Swift. Like an "if not ..."
+        puts "#{task.to_string}"
+      end
+    end
+  end
+
+  # rather than having user type in a task, they will choose the task from their task list, hence this method
   def ask_for_task
-    print_tasks
+    print_all_tasks
 
     print "Task Number [Between 1 and #{@task_list.length}]: "
-    gets.chomp.to_i - 1
+    gets.chomp.to_i - 1 # read input (gets), remove newline char (chomp), convert to integer (to_i), subtract 1 for correct index. Again, no return keyword
   end
 
   def add_task(task)
-    @task_list << task
+    @task_list << task # << is the append operator. Can be used for arrays or strings
   end
 
-  # static method that resembles functional programming style
+  # static method that resembles functional programming style. Yes, there is a better way to do this, but this is for demonstration purposes
   def self.search_task(task_list, title, idx)
     current = task_list[idx]
 
     if idx >= task_list.length
       puts "Could not find task: '#{title}'"
-    elsif current.title.eql? title
+    elsif current.title.eql? title # compares strings. "==" also works, but it doesn't compare types
       puts "Found task: '#{title}'"
     else
       search_task(task_list, title, idx+1)
@@ -74,19 +89,20 @@ class TaskList
   end
 end
 
-
-task_list = TaskList.new
-puts "Welcome to the Ruby Task Manager!"
+############################################# TASK MANAGER EXECUTION #############################################
+task_list = TaskList.new # instantiate object. If constructor has no parameters, then can call "new" without parentheses
+puts "Welcome to the Ruby Task Manager!" # "puts" automatically creates newline
 
 while true
-  print "Please select a desired action:\n\tprint\n\tadd\n\tsearch\n\tupdate\n\tremove\nDesired action: "
+  print "\nPlease enter a desired action:\n\tprint\n\tadd\n\tsearch\n\tupdate\n\tcomplete\n\tshow_incomplete\n\tremove\nDesired action: " # "print" doesn't create newline
   choice = gets.chomp
 
   case choice
   when "print"
-    task_list.print_tasks
+    task_list.print_all_tasks
+
   when "add"
-    puts "Please answer the following prompts"
+    puts "Please answer the following prompts:"
     print "\tTask title: "
     title = gets.chomp
 
@@ -96,16 +112,19 @@ while true
     task = Task.new(title, description)
     task_list.add_task(task)
 
+    puts "\nSuccessfully added task"
+
   when "search"
     print "Task to search: "
     title = gets.chomp
 
-    TaskList.search_task(task_list.task_list, title, 0)
+    TaskList.search_task(task_list.task_list, title, 0) # static method call
 
   when "update"
     idx = task_list.ask_for_task
 
     puts "Please select a field to update:\n\ttitle\n\tdescription"
+    print "Field: "
     field = gets.chomp
 
     print "New Content: "
@@ -113,9 +132,23 @@ while true
 
     task_list.update_task(idx, field, updated_content)
 
+    puts "\nSuccessfully updated task"
+
+  when "complete"
+    idx = task_list.ask_for_task
+    task = task_list.task_list[idx]
+
+    task.mark_complete
+    puts "\n#{task.title} has been marked complete"
+
+  when "show_incomplete"
+    task_list.print_incomplete_tasks
+
   when "remove"
     idx = task_list.ask_for_task
     task_list.remove_task(idx)
+
+    puts "\n#{task_list.task_list[idx].title} has been removed"
   else
     puts "Invalid action"
   end
